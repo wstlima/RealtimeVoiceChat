25eeb2f7d91cf45c05e2bbbdd5a9f53c498abf13
WSLima <WSLima@br.atento.com>
2025-08-25 00:04:35 -0300

    iniciando ajustes

--- code/server.py
+++ code/server.py
@@ -32,7 +32,7 @@ TTS_START_ENGINE = "kokoro"
 TTS_ORPHEUS_MODEL = "orpheus-3b-0.1-ft-Q8_0-GGUF/orpheus-3b-0.1-ft-q8_0.gguf"
 
 LLM_START_PROVIDER = "ollama"
-LLM_START_MODEL = "hf.co/bartowski/Llama-3.2-3B-Instruct-GGUF:Q8_0"
+LLM_START_MODEL = "huggingface.co/bartowski/Llama-3.2-3B-Instruct-GGUF:Q8_0"
 NO_THINK = False
 DIRECT_STREAM = TTS_START_ENGINE=="orpheus"
 
